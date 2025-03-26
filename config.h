3f92f9bc6b5036e4c3fedd02fad0d8c9d6c83d62
Gustav Hogbring <gustav.hogbring@proton.me>
2025-03-26 19:59:36 +0100

    update

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 4;               /* add an defined amount of pixels to the bar height */
+static const int user_bh = 8;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
 	"monospace:size=12"
